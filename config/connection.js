b5e1c4e680308a64378148ae03b0a24f7e00f0fb
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-07-03 17:25:09 -0700

    attempt #4 jawsDB bug fix

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,14 @@ let connection;
 if (process.env.JAWSDB_URL) {
     // Database is JawsDB on Heroku
     connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else if (process.env.PORT) {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'p85wxsjts1341y30',
+        password: 'yi6z1xh2f0q017rw',
+        database: 'rlelx6qyre1np09o'
+    })
 } else {
     // Database is local
     connection = mysql.createConnection({
